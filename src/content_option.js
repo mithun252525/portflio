ac3631338e164a80a178c3258eabd1b5b162b32e
mithun252525 <86620307+mithun252525@users.noreply.github.com>
2022-01-14 16:42:30 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,106 @@
+const logotext = "MEDHUN C";
+const meta = {
+    title: "Medhun Portflio ",
+    description: "Student",
+};
+
+const introdata = {
+    title: "M3DHUN C",
+    animated: {
+        first: "if Bug is a Human",
+        second: "to Debug is Divine",
+        third: "while!(succeed = try()))",
+    },
+    description: "Web Developer",
+    your_img_url: "https://media-exp1.licdn.com/dms/image/C5603AQE8VlnFCBSRHg/profile-displayphoto-shrink_400_400/0/1641644787218?e=1646870400&v=beta&t=Z0VsFGevEbJZszPh9OHjukAS3gWGQEwSBoWNsOe5rII",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "Myself medhun and i am from Tamil nadu.I have graduating my B.E in the stream of computer science & engineering from Dr.MCET.In day to day life Learning a new Technologies that improve my knowledge and to enhance my Skills.opportunity doesn't knock,build a door to step in........... ",
+};
+const Educationtimeline = [{
+        jobtitle: "Navarasam Matric higher secondary school,palliyuthu",
+        where: "10 Standard",
+        date: "2016",
+    },
+    {
+        jobtitle: "Navarasam Matric higher secondary school,palliyuthu",
+        where: "12 standard",
+        date: "2019",
+    },
+    {
+        jobtitle: "Dr.Mahalingam college of engineering and technology,pollachi",
+        where: "B.E CSE",
+        date: "2018-2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "HTML & CSS",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React.js",
+        value: 60,
+    },
+    {
+        name: "MYSql",
+        value: 85,
+    },
+];
+
+const services = [
+ {
+        title: "Courses",
+        description: "Javascript, Block Chain Basics",
+    },
+
+{
+
+        title: "Skills",
+        description: "Web Development in react",
+    },
+     {
+        title: "Project Work",
+        description: "Effective Crop Selection using Machine Learning. , Real time Hand Gesture Recoginition using openCV.",
+    },
+
+    
+];
+
+
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    Educationtimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
